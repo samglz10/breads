1837e0ada545e71c221bf8830c024be10a012d87
Samuel Gonzalez <samglz10@gmail.com>
2022-06-15 19:24:23 -0400

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -7,6 +7,7 @@ breads.get('/', (req, res) => {
     res.render('Index',
       {
         breads: Bread
+        //title:'Index Page'
       }
     )
   // res.send(Bread)
@@ -14,6 +15,15 @@ breads.get('/', (req, res) => {
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-  })
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.send('404')
+  }
+})
+
+
+  
 module.exports = breads
--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const express = require('express')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+
 // MIDDLEWARE
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
@@ -18,6 +20,12 @@ app.get('/', (req, res) => {
 // Breads
   const breadsController = require('./controllers/breads_controller.js')
   app.use('/breads', breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
   
 // LISTEN
 app.listen(PORT, () => {
